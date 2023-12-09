58aac92094285218a24a22bea44549d1cf4659ab
mercy-wumi <m.adams1909@gmail.com>
2023-12-09 11:11:32 +0100

    abit of setup to get started - I add some color in the tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      primaryYellow: "#FFC700",
+      gray: "#C0C0C0",
+      primaryBlack: "#191919",
+      darkGray: "#282828",
+    },
+  },
+  plugins: [],
 };
