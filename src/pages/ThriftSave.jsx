becb9685d2e3fae5c3fe9f2cb59ac6448e045299
mercy-wumi <m.adams1909@gmail.com>
2023-12-10 07:32:34 +0100

    dashboard pages cont'd

--- src/pages/ThriftSave.jsx
+++ src/pages/ThriftSave.jsx
@@ -2,14 +2,17 @@ import save from "../assets/save.svg";
 
 const ThriftSave = () => {
   return (
-    <div className="flex">
-      <img src={save} alt="save" />
-      <p>You currently dont have any thrift savings</p>
-      <div>
-        <button>Start New Thrift</button>
-        <button>Join Thrift</button>
+    <>
+      <Header title="Welcome back, Cynthia!" />
+      <div className="flex">
+        <img src={save} alt="save" />
+        <p>You currently dont have any thrift savings</p>
+        <div>
+          <button>Start New Thrift</button>
+          <button>Join Thrift</button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
