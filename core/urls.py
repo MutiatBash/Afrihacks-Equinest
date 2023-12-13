491a6816f81e1f4e991833a56b8feaa1ea679d73
jaypee15 <okoyejohnpaul15@gmail.com>
2023-12-13 02:44:02 +0100

    fe

--- core/urls.py
+++ core/urls.py
@@ -9,8 +9,8 @@ SpectacularSwaggerView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
-    path("api/dj-rest-auth/", include("dj_rest_auth.urls")),
-    path("api/dj-rest-auth/registration/", 
+    path("api/", include("dj_rest_auth.urls")),
+    path("api/registration/", 
         include("dj_rest_auth.registration.urls")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/schema/redoc/", SpectacularRedocView.as_view(
